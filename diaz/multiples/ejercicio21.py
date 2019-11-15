d6be348327cbc4866946abd726c9b17ccbb2cf9a
docente-unprg <57265069+docente-unprg@users.noreply.github.com>
2019-11-15 11:57:07 -0500

    Se agrego el bloque de ejercicio simples y multiples

--- diaz/multiples/ejercicio21.py
+++ diaz/multiples/ejercicio21.py
@@ -6,9 +6,10 @@ monto=float(os.sys.argv[2])
 # Si el monto supera los 100 soles, mostrar tarjeta dorada
 if ( monto > 100 ):
     print("Ud ha ganado la tarjeta Dorada")
-else:
+if ( monto >= 90 and monto < 100):
+    print("Ud casi casi gana la tarjeta")
+if ( monto < 90):
     print("Vuelva pronto")
-#fin_if
 
 print(cliente)
 print(monto)
--- diaz/simples/ejercicio01.py
+++ diaz/simples/ejercicio01.py
@@ -6,8 +6,6 @@ monto=float(os.sys.argv[2])
 # Si el monto supera los 100 soles, mostrar tarjeta dorada
 if ( monto > 100 ):
     print("Ud ha ganado la tarjeta Dorada")
-else:
-    print("Vuelva pronto")
 #fin_if
 
 print(cliente)
